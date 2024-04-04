8fb017a3bbfe45fef5f188c786ce8d2a79eaa7ac
marcbull <31337222+marcbull@users.noreply.github.com>
2024-04-04 09:30:48 +0200

    fix: fix git repository in liblzma port

--- liblzma/portfile.cmake
+++ liblzma/portfile.cmake
@@ -1,7 +1,8 @@
-vcpkg_from_github(
+vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO tukaani-project/xz
-    REF "v${VERSION}"
+    URL https://git.tukaani.org/xz.git
+    REF 11fa4ea8f8e131ac5b100a25703ee86a40590339
+    FETCH_REF "v${VERSION}"
     SHA512 c28461123562564e030f3f733f078bc4c840e87598d9f4b718d4bca639120d8133f969c45d7bdc62f33f081d789ec0f14a1791fb7da18515682bfe3c0c7362e0
     HEAD_REF master
     PATCHES
